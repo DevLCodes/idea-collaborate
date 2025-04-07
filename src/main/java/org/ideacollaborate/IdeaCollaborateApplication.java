9feb3cca7b06bc482f1ff0e3baa5a4ce70c642a5
DevLCodes <lkumar@clari.com>
2025-04-07 20:16:16 +0530

    Initial Commit

---
+++ src/main/java/org/ideacollaborate/IdeaCollaborateApplication.java
@@ -0,0 +1,13 @@
+package org.ideacollaborate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaCollaborateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaCollaborateApplication.class, args);
+    }
+
+}
